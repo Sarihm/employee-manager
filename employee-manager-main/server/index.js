c5acb71c4330e4e2c3f4aa66cbd044ae1b2e9534
Sarihm <30509330+Sarihm@users.noreply.github.com>
2021-03-14 22:14:11 -0600

    Add files via upload

--- employee-manager-main/server/index.js
+++ employee-manager-main/server/index.js
@@ -10,7 +10,7 @@ const cookSession = require('cookie-session')
 
 // Importing our Login Service Used With the POST Login Route
 const loginService = require('./services/loginService')
-
+const departmentRoutes = require('./routes/departmentRouter')
 
 
 // create an instance of express
@@ -21,14 +21,15 @@ const app = express()
 // we can set configuration variables for the application.
 // never upload to git...
 const PORT =  process.env.PORT || 5000 
-
+ 
  
 // Middleware For Cross Origin Resource SHaring
 app.use(cors())
 
 //To get access to the name value pairs send in the message Body of POST Request.
- app.use(express.urlencoded({extended:true}))
+ app.use(express.urlencoded({extended:false}))
  app.use(express.json())
+ app.use(express.raw())
 
  // Session Middleware
  app.use(cookSession({
@@ -45,7 +46,7 @@ app.use(cors())
 // second parameter is an configuration object of how we want
 // the static file server to run.
  
-app.use(express.static(path.join(__dirname, "../client"), {extensions: ["html", 'htm']})
+app.use(express.static(path.join(__dirname, `../www`), {extensions: ["html", 'htm']})
 );
 
  
@@ -100,22 +101,15 @@ app.use(express.static(path.join(__dirname, "../client"), {extensions: ["html", 
        }
   })
     
- 
- app.post('/login', (req, res)=>{
-   // POST name value pairs in body request
-   const credentials = {
-     email:req.body.email,
-     password:req.body.password
-    }
-    
-    
-    const isValidUser = loginService.authenticate(credentials)
-   
-    res.end()
- 
+ app.get('/users', (req, res)=>{
+     // read using the file service
+     // return as json
+     const date = fileService.readFile('../data/user.json')
+     res.json(data)
  })
 
- 
+
+ app.use('/api/departments', departmentRoutes())
 
 // Final Middleware 
 // Catch all for any request not handled while express was
